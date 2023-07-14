fa195febd0135c73a48c5bb47ffdc8dfcb831cb5
GodoyMS <peruvalmundial@gmail.com>
2023-07-14 00:50:06 -0500

    changed permiision2S

--- src/collections/ProfilesUpload.ts
+++ src/collections/ProfilesUpload.ts
@@ -17,15 +17,22 @@ const ProfilesUpload: CollectionConfig = {
     {
       path: "/upload",
       method: "post",
-      handler: async (req, res, next) => {
-        const { base64, name } = req.body;
-        const { data, url } = await UploadImageBucket(base64, name);
+      handler: async (req, res) => {
 
-        if (data) {
-          res.status(200).send({ message: "Uploaded", url });
-        } else {
-          res.status(404).send({ error: "not found" });
+        try {
+          const { base64, name } = req.body;
+          const { data, url } = await UploadImageBucket(base64, name);
+  
+          if (data) {
+            res.status(200).send({ message: "Uploaded", url });
+          } else {
+            res.status(404).send({ error: "not found" });
+          }
+          
+        } catch (error) {
+          console.log(error)
         }
+    
       },
     },
   ]
